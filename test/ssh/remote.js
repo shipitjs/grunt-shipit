80bd20377ece33d35984be2b3a27bb190de2e778
Greg Bergé <neoziro@gmail.com>
2013-11-12 23:54:32 +0100

    Use "deploy" as default remote user.

--- test/ssh/remote.js
+++ test/ssh/remote.js
@@ -6,6 +6,10 @@ describe('SSH remote', function () {
     it('should parse remote', function () {
       expect(remote.parse('user@host')).to.deep.equal({ user: 'user', host: 'host' });
     });
+
+    it('should use deploy as default user', function () {
+      expect(remote.parse('host')).to.deep.equal({ user: 'deploy', host: 'host' });
+    });
   });
 
   describe('#format', function () {
